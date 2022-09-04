09da060362e0ba144ce74cd17448ecd1669a6e2f
emma673r <90177139+emma673r@users.noreply.github.com>
2022-09-04 19:24:23 +0200

    display and all images re-done pb with loop resolved

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ const allCleanStudents = [];
 
 const Student = {
   firstName: "",
-  middleName: "unknow middle name",
-  nickName: "unknown nick name",
+  middleName: "unknown",
+  nickName: "unknown",
   lastName: "",
-  imageSrc: "image.png",
+  imageSrc: "",
   house: "",
 };
 
 const studentList = document.getElementById("student_list");
-const template = document.getElementById("student").content;
+const template = document.getElementById("student");
 // const singleStudent = document.querySelectorAll(".single_student");
 
 const url = "https://petlatkea.dk/2021/hogwarts/students.json ";
@@ -23,36 +23,27 @@ const url = "https://petlatkea.dk/2021/hogwarts/students.json ";
 // all my variables
 
 let list;
+// ****** CONTROLLER
+//* step 1 : start
 
 // start
 function start() {
   //   console.log(`start`);
   fetchData();
 }
+// step 2 : fetch data
 
-// ****** CONTROLLER
-//* step 1 : async fetch data
 async function fetchData() {
   const response = await fetch(url);
   list = await response.json();
 
-  //   console.log(`allStudents is :`, list);
+  // console.log(`allStudents is :`, list);
 
-  prepareStudents(list);
+  cleanStudentData();
 }
 
 // ****** MODEL
 //* step 2 : init loop for each student aka prepare the student list
-function prepareStudents(list) {
-  //   console.log(`prepareStudents`);
-
-  //   console.table(list);
-  //   foreach student
-  list.forEach((student) => {
-    // console.log(`forEach working`);
-    cleanStudentData();
-  });
-}
 
 // *step 3 clean the data
 function cleanStudentData() {
@@ -101,15 +92,14 @@ function cleanStudentData() {
 
     // if middle name is empty display undefined middle name
     if (middleName === "") {
-      student.middleName = "undefined middle name";
-    } else {
+      student.middleName = "No middle name";
+    } else if (middleName.includes(" ")) {
       // if middle includes a space - replace space with empty character (" " to "")
-      if (middleName.includes(" ")) {
-        middleName = middleName.replace(" ", "");
-      }
+      middleName = middleName.replace(" ", "");
+    } else {
       // uppercase first letter
       middleName = middleName[0].toUpperCase() + middleName.substring(1);
-      console.log(`middleName is _${middleName}_`);
+      //   console.log(`middleName is _${middleName}_`);
       student.middleName = middleName;
     }
     // console.log(student);
@@ -117,12 +107,16 @@ function cleanStudentData() {
     // ! SHOULD : find string inside of "" and isolate + display undefined if student doesnt have one
     // only one student with nickname
     // nickname displayed by putting it between ""
+    let nickName;
 
-    let nickName = fullName.substring(fullName.indexOf(`"`) + 1, fullName.lastIndexOf(`"`));
-    nickName = nickName.charAt(0).toUpperCase() + nickName.substring(1)
-    student.nickName = nickName;
+    if (!nickName) {
+      student.nickName = "No nick name";
+    } else {
+      nickName = fullName.substring(fullName.indexOf(`"`) + 1, fullName.lastIndexOf(`"`));
+      nickName = nickName.charAt(0).toUpperCase() + nickName.substring(1);
+      student.nickName = nickName;
+    }
     // !somehow nickName[0] is not defined ???? dont understand
-
 
     // ** 3.2.4 lastName
     let lastName = fullNameArray[fullNameArray.length - 1];
@@ -142,7 +136,7 @@ function cleanStudentData() {
     // ! SHOULD : have the right name to fetch the right photo from right depository
     //* look at images and student names - find a pattern
     //* all images src have last name written, a _ and first letter firstname
-    //* exceptions are : Leanne and the Patils
+    //* exceptions are : Leanne and the Patils and finch-fletchey
 
     // if student name has no last name - Leanne
     if (!fullName.includes(" ")) {
@@ -152,6 +146,10 @@ function cleanStudentData() {
     // so if student name includes patil
     else if (fullName.toLowerCase().includes("patil")) {
       imgSrc = `./images/${lastName.toLowerCase()}_${firstName.toLowerCase()}.png`;
+    }
+    // if student includes "-" - finch fletchey
+    else if (fullName.includes("-")) {
+      imgSrc = `./images/${fullName.substring(fullName.lastIndexOf("-") + 1).toLowerCase()}_${student.firstName[0].toLowerCase()}.png`;
     }
     // if student name has last name and first name and is only one with last name (the OG pattern)
     else {
@@ -165,28 +163,43 @@ function cleanStudentData() {
     house = house[0].toUpperCase() + house.substring(1);
     student.house = house;
 
-    console.log(student);
+    // console.log(student);
+    //* PUSH each elm in new clean student array :   allCleanStudents.push(elm)
+    allCleanStudents.push(student);
+    // console.log(allCleanStudents);
+    console.log(student.imgSrc);
   });
-
-  //* PUSH each elm in new clean array :   allCleanStudentList.push(elm)
+  //   console.log(allCleanStudents);
+  displayStudents();
 }
 
 // ***** VIEW
 //* step 4 display/show the data
-function displayStudents() {
+function displayStudents(student) {
   console.log(`displayStudents`);
+  //   console.table(allCleanStudents)
   // ** 4.1  foreach student loop
+  allCleanStudents.forEach((student) => {
+    // ** 4.2  CLONE const clone = template cloneNode(true)
+    const clone = template.content.cloneNode(true);
+    //* 4.2.0 clone h2 data field fullname = firstname + middle name + last name
+    clone.querySelector("[data-field=fullName]").innerHTML = `${student.firstName} ${student.lastName}´s student card`; // ** 4.2.1  clone qS [data-field="firstName"] = student.firstName
+    clone.querySelector("[data-field=firstName]").innerHTML = `<u>First name</u> : <b>${student.firstName}</b>`;
+    // ** 4.2.2  clone qS [data-field="MiddleName"] = student.middleName
+    clone.querySelector("[data-field=middleName]").innerHTML = `<u>Middle name</u> : <b>${student.middleName}</b>`;
+    // ** 4.2.3  clone qS [data-field="NickName"] = student.NickName
+    clone.querySelector("[data-field=nickName]").innerHTML = `<u>Nick name</u> : <b>${student.nickName}</b>`;
+    // ** 4.2.4  clone qS [data-field="LastName"] = student.lastName
+    clone.querySelector("[data-field=lastName]").innerHTML = `<u>Last name</u> : <b>${student.lastName}</b>`;
+    // ** 4.2.5  clone qS [data-field="gender"] = student.gender
+    clone.querySelector("[data-field=gender]").innerHTML = `<u>Gender</u> :<b>${student.gender}</b>`;
+    // ** 4.2.6  clone qS [data-field="imgSrc"].src = student.imgSrc
+    clone.querySelector("[data-field=imgSrc]").src = student.imgSrc;
+    clone.querySelector("[data-field=imgSrc]").alt = `picture of ${student.lastName}, ${student.firstName}`;
+    // ** 4.2.7  clone qS [data-field="house"] = student.house
+    clone.querySelector("[data-field=house]").innerHTML = `<u>House</u> : <b>${student.house}</b>`;
 
-  // ** 4.2  CLONE const clone = template cloneNode(true)
-  // ** 4.2.1  clone qS [data-field="firstName"] = student.firstName.value (?? do you need the value at the end, test)
-  // ** 4.2.2  clone qS [data-field="MiddleName"] = student.middleName
-  // ** 4.2.3  clone qS [data-field="NickName"] = student.NickName
-  // ** 4.2.4  clone qS [data-field="LastName"] = student.lastName
-  // ** 4.2.5  clone qS [data-field="gender"] = student.gender
-  // ** 4.2.6  clone qS [data-field="imgSrc"].src = student.imgSrc
-  // ** 4.2.7  clone qS [data-field="house"] = student.house
-
-  // ** 4.3 APPEND studentList.appenChild(clone);
-
-  //   end
+    // ** 4.3 APPEND studentList.appenChild(clone);
+    studentList.appendChild(clone);
+  });
 }
